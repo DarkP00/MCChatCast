9f41c6ac1a1265a901cd84f03af8a439bca62ef0
Subject 3910 <laythghoul@gmail.com>
2024-07-08 19:43:10 +0600

    Wrote Readme, Made command ressive mechanics from discord (prifix >), Made command exicute mechanics to mc server, Made test cast mechanics

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-Using asyncssh for SSH accsess and MCRcon for get responce from mc console.
+Using MCRcon for mc remoteconsole.
 
 Minecraft End:
+    ---> Go to server.properties
+    ---> Add or modefy this lines
+            enable-rcon=true
+            rcon.port=25575
+            rcon.password=your_rcon_password
+    ---> Restart the server        
+    ---> Add the bot to your discrod server with the link given in the discord
 
 Bot End:
+    ---> Change to currect values in bot.py
+    ---> Set bot token in the discord
+    ---> Run the pogram
+
+    ---> Add more cmd in cmd.json if you want
--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import asyncssh
 import mcrcon
+from mcrcon import MCRcon
 import os
 import json
 
@@ -11,11 +11,6 @@ Bot_Token = ''
 Rcon_Host = ''
 Rcon_Pass = ''
 Rcon_Port = ''
-
-SSH_Host = ''
-SSH_Name = ''
-SSH_Pass = ''
-SSH_Port = ''
 
 #reding valid cmds
 with open('Asstes/cmd.json', 'r') as cmd:
@@ -39,16 +34,57 @@ async def on_message(message):
     if message.content.startswith('>'):
         contents = message.content[1:].split(' ')
         author = message.author
-        print(f"\033[32m{message.suther} run the command {contents}\033[0m")
+        print(f"\033[32m{author} run the command {contents}\033[0m")
         if contents in cmds['commands']:
             commands = cmds['commands'][contents]
             if "__player__" in commands:
                 commands.replace("__player__", author)
         print(f"\033[32m{commands}\033[0m")
 
+        # Opening SSH and Writing commands
+        with MCRcon(Rcon_Host, Rcon_Pass, port=Rcon_Port) as comm:
+            result = comm.command(commands)
+            print(result)
 
-        
+# Message discord to server
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
+    if not message.content.startswith('>'):
+        contents = message.content[1:].split(' ')
+        author = message.author
+        await print(f"\033[32m{author} sent the message {contents}\033[0m")
 
+        # Opening SSH and Writing commands
+        with mcrcon(Rcon_Host, Rcon_Pass, port=Rcon_Port) as comm:
+            result = comm.command(f"say @a {author} said, {contents}")
+            await print(result)
+
+# Copy and Prossess the chat
+def chat_prossess(response):
+    messages = []
+    lines = response.split('\n')
+    for line in lines:
+        if 'INFO' in line and '<' in line and '>' in line:
+            timestamp, content = line.split('INFO]: ')
+            messages.append(content)
+    return messages
+
+# Send Chat to Discord with mcrcon
+def send_chet_to_discord():
+    try:
+        with MCRcon(Rcon_Host, Rcon_Pass, port=Rcon_Port) as comm:
+            response = comm.command(LOG_COMMAND)
+            messages = chat_prossess(response)
+            for message in messages:
+                message.channel.send(message)
+    except Exception as e:
+        print(f"erorr: {e}")
+
+# Run send_chet_to_discord ficntion
+if __name__ == "__send_chet_to_discord__":
+    send_chet_to_discord()
 
 bot.run(Bot_Token)
